4c1dc36abca9ae765ca11779041f5cea7566c52b
Blex <iamlex.tk@gmail.com>
2024-11-04 00:27:59 +0100

    mongo math fist usefullly version 3

--- index.js
+++ index.js
@@ -2,16 +2,13 @@ const MongoAnalyzer = require("./mongoMath");
 const MongoStats = require("./mongoSpecificAnalyser.js");
 const MongoInterpreter = require("./mongoInterpreter.js");
 const MongoNlAnalyser = require("./mongoNlanalyser.js");
-
 class MongoMath {
   constructor(uri) {
     this.uri = uri;
   }
-
   async dataAnalyzer(params) {
     const analyzer = new MongoAnalyzer({ uri: this.uri });
     const stats = new MongoStats({ uri: this.uri });
-
     try {
       await analyzer.connect();
       const results = await analyzer.analyzeDatabaseComplete(params);
@@ -21,14 +18,12 @@ class MongoMath {
     } finally {
       await analyzer.disconnect();
     }
-
     try {
       await stats.connect();
       const generalStats = await stats.calculateStatistics({
         collection: params.collection,
       });
       console.log("General Statistics:", generalStats);
-
       if (params.crossFieldAnalysis) {
         const crossStats = await stats.calculateCrossFieldStatistics(
           params.collection,
